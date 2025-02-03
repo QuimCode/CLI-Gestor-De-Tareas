ccdba7a32307906736498aa72284ca20ab1378f9
QuimCode <quimafoncode@gmail.com>
2025-02-03 20:32:31 -0300

    TM V0.32

--- Menu/CLI_Menus.py
+++ Menu/CLI_Menus.py
@@ -1,5 +1,9 @@
 import Logica.CLI_Logica as logica
 
+def mostrar_menu():
+    # Funcion de ejecucion de los menus
+    menu_inicial()
+
 def menu_inicial():
     # Funcion de presentacion solo textual de las comandos
     "Bienvenido a TareaManager(TM) la mejor aplicación para gestionar tus tareas sin impedimentos."
@@ -11,6 +15,13 @@ def menu_registro():
     datos_usuario = logica.registrarse()
     return datos_usuario
 
-def mostrar_menu():
-    # Funcion de ejecucion de los menus
-    menu_inicial()
+def menu_app():
+    # Funcion de presentacion solo textual de las comandos
+    print("1. Crear tarea")
+    print("2. Editar tarea")
+    print("3. Eliminar tarea")
+    print("4. Listar tareas terminadas")
+    print("5. Listar tareas pendientes")
+    print("6. Listar tareas no terminadas")
+    print("7. Listar todas las tareas")
+    print("8. Volver al menu principal")
